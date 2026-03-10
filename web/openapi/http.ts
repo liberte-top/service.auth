2bc55ebee8161aef7567c9d385564c6fe9f16259
PerishCode <perishcode@gmail.com>
2026-03-10 10:38:51 +0800

    refactor service.auth delivery for same-origin deployment

--- web/openapi/http.ts
+++ web/openapi/http.ts
@@ -1,8 +1,6 @@
 import axios, { type AxiosRequestConfig } from "axios";
 
-const baseURL = import.meta.env.VITE_AUTH_API_BASE_URL ?? "http://localhost:3333";
-
-const AXIOS = axios.create({ baseURL });
+const AXIOS = axios.create();
 
 export async function customInstance<T>(config: AxiosRequestConfig): Promise<T> {
   const response = await AXIOS.request<T>(config);
